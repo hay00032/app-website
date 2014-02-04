7155f274580056513b05917e2216b6e21aeb0cc6
hay00032 <coulross@Ross-MacBook.local>
2014-02-03 21:23:52 -0500

    video adjustments

--- _site/js/video.js
+++ _site/js/video.js
@@ -2,7 +2,8 @@ var $videoDialog = $('dialog');
 dialogPolyfill.registerDialog($videoDialog.get(0));
 
 $('.btn-open').on('click', function () {
-	$videoDialog.children('.video').html('<iframe src=http://player.vimeo.com/video/81359622?color=030303&amp;autoplay=1" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+	$videoDialog.children('.video-js').html('<iframe src=http://player.vimeo.com/video/81359622?color=030303&amp;autoplay=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>')
+
 	/*
 	When triggering built-in Javascript functions we need to bypass jQuery
 	In order to get to the raw Javascript element we can use .get()
@@ -18,6 +19,7 @@ $('.btn-open').on('click', function () {
 		.show() allows users to interact with stuff behind the dialog
 		.showModal() disables user interaction behind the dialog
 	*/
+	$videoDialog.css('width', '80%');
 })
 
 $('.btn-close').on('click', function (){
