9be76dbefdfedfb90e920d6bdfd31c7549f2b05c
hay00032 <coulross@Ross-MacBook.local>
2014-02-03 22:41:57 -0500

    added js animations

---
+++ _site/js/animation.js
@@ -0,0 +1,26 @@
+var $win = $(window);
+var $sun = $('.sun');
+var $dipper = $('.dipper');
+var $ships = $('.ships img');
+
+$win.on('scroll', function (){
+	//console.log($win.scrollTop());
+	$sun.css('transform', 'rotate(' + $win.scrollTop() /5 + 'deg)')
+
+});
+
+$('.logo-section').waypoint(function () {
+	$logo.addClass('js-logo-animate');
+}, { offset: '60%' });
+
+$('.phone-section').waypoint(function () {
+	$phone.addClass('js-phone-animate');
+}, { offset: '60%' });
+
+$('.ships-section').waypoint(function () {
+	$ships.eq(0).addClass('js-ship-delay-1');
+	$ships.eq(2).addClass('js-ship-delay-3');
+	$ships.eq(3).addClass('js-ship-delay-2');
+	$ships.addClass('js-ships-animate');
+}, { offset: '60%' });
+
